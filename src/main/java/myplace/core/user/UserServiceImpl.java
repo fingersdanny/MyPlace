7e63ccbdf10f2492c31d6a0cd5cadf98612f3ecc
isevou__ <kbry12@naver.com>
2023-06-23 00:32:21 +0900

    add : create user method 상속 및 @Override

--- src/main/java/myplace/core/user/UserServiceImpl.java
+++ src/main/java/myplace/core/user/UserServiceImpl.java
@@ -1,17 +1,16 @@
 package myplace.core.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@RequiredArgsConstructor
+@Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
     @Override
     public void join(User user) {
         userRepository.save(user);
@@ -20,6 +19,17 @@ public class UserServiceImpl implements UserService{
     @Override
     public Optional<User> findUser(Long id) {
         return userRepository.findById(id);
+    }
+
+    @Override
+    public User create(String name, String username, String email, String password) {
+        User user = new User();
+        user.setName(name);
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        this.userRepository.save(user);
+        return user;
     }
 
     public UserRepository getUserRepository() {
