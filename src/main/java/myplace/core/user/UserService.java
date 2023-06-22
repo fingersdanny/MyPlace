997b1bbaad9517adc8032d7d2a549a9fd6882642
isevou__ <kbry12@naver.com>
2023-06-23 00:31:25 +0900

    add : create user method

--- src/main/java/myplace/core/user/UserService.java
+++ src/main/java/myplace/core/user/UserService.java
@@ -6,4 +6,6 @@ public interface UserService {
     void join(User user);
 
     Optional<User> findUser(Long id);
+
+    public User create(String name, String username, String email, String password);
 }
