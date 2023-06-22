7fdb88b09f4eb05108abbdd7979458261df5078a
isevou__ <kbry12@naver.com>
2023-06-22 22:49:11 +0900

    add : MainController

---
+++ src/main/java/myplace/core/controller/MainController.java
@@ -0,0 +1,10 @@
+package myplace.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.web.bind.anno
+
+@Controller
+public class MainController {
+
+    @GetMapping
+}
