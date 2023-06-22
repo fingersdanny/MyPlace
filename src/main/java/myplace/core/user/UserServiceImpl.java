dd10f590abca539ba976dbda55b64e40f6c8b29a
isevou__ <kbry12@naver.com>
2023-06-23 01:53:12 +0900

    add : 생성자 주입으로 변경

--- src/main/java/myplace/core/user/UserServiceImpl.java
+++ src/main/java/myplace/core/user/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package myplace.core.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public void join(User user) {
