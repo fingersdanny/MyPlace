81b46608a9a1589b870c8a41cade2e3b16dc8524
isevou__ <kbry12@naver.com>
2023-06-23 00:23:44 +0900

    add : UserServiceImpl

---
+++ src/main/java/myplace/core/user/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package myplace.core.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public void join(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findUser(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public UserRepository getUserRepository() {
+        return userRepository;
+    }
+}
