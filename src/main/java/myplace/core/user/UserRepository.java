bf2033f3274e6a3fdb97d01c9dbc10bd01707b85
isevou__ <kbry12@naver.com>
2023-06-23 01:11:08 +0900

    add : @Component 추가

--- src/main/java/myplace/core/user/UserRepository.java
+++ src/main/java/myplace/core/user/UserRepository.java
@@ -1,10 +1,13 @@
 package myplace.core.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Component
+public interface UserRepository extends CrudRepository<User, Long> {
     User save(User member);
     Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
