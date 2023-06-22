b0e8ab0a13224b0d69d4d2fdf8b12beaafde9fda
isevou__ <kbry12@naver.com>
2023-06-23 01:52:13 +0900

    add : UserRepository @Bean 추가

--- src/main/java/myplace/core/AppConfig.java
+++ src/main/java/myplace/core/AppConfig.java
@@ -1,10 +1,16 @@
 package myplace.core;
 
+import myplace.core.user.MySqlUserRepository;
+import myplace.core.user.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
-
+    @Bean
+    public UserRepository userRepository() {
+        return new MySqlUserRepository();
+    }
 }
