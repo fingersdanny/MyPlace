9633120b14a07a67bafab881dc574c1659899da3
isevou__ <kbry12@naver.com>
2023-06-23 00:13:55 +0900

    add : UserRepository 상속관계 수정

--- src/main/java/myplace/core/user/UserRepository.java
+++ src/main/java/myplace/core/user/UserRepository.java
@@ -1,8 +1,10 @@
 package myplace.core.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     User save(User member);
     Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
