eb9e1a59979cf1f6ff200ac1071d4fb2f37cf7f0
isevou__ <kbry12@naver.com>
2023-06-22 22:36:13 +0900

    add : Member에 Jpa 적용

--- src/main/java/myplace/core/member/Member.java
+++ src/main/java/myplace/core/member/Member.java
@@ -1,12 +1,24 @@
 package myplace.core.member;
 
+
 import lombok.Data;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Data
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String username;
     private String password;
+    @Column(unique = true)
     private String email;
 }
